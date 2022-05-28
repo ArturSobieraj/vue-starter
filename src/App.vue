9a2ae738d1bd83b02cb2c18621a402cbea17b8bb
student8 <student8@pcoip.ki.agh.edu.pl>
2022-05-28 14:12:59 +0200

    Next one

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div id="app">
     <h1>Twój e-mail to: {{email}}</h1>
-    <input type="email" v-model="email">
+    <div v-if="email.length < 10">Krótki</div>
+    <div v-else-if="email.length < 15">OK</div>
+    <div class="bad" v-else>Za długi</div>
+    <input type="email" v-on:keyup.enter="alertMyEmail" v-model="email">
+    <button @click="alertMyEmail()">Wyśwetl email</button>
   </div>
 </template>
 
@@ -12,11 +16,18 @@ export default {
     return {
       email: ''
     }
+  },
+  methods: {
+    alertMyEmail() {
+      alert(this.email);
+    }
   }
 }
 
 </script>
 
 <style>
-
+.bad {
+  color: red;
+}
 </style>
